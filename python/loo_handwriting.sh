22f174e95f3a074ec9fa928b1ded1af76c034490
corruptbear <wawnx716@gmail.com>
2025-05-22 22:57:37 -0700

    adaptive python version in script

--- python/loo_handwriting.sh
+++ python/loo_handwriting.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Detect OS and set Python executable
+if [[ "$(uname)" == "Darwin" ]]; then
+    PYTHON_EXEC=python3.10
+else
+    PYTHON_EXEC=python
+fi
+
 # Define the full list of users
 users=(1 8 9)
 
 # Loop over each user as the test user
 for test_user in "${users[@]}"; do
-    rm ./datasets_audio/augmented_dataset.pkl
-    rm ./datasets_audio/processed_dataset.pkl
+    rm -f ./datasets_audio/augmented_dataset.pkl
+    rm -f ./datasets_audio/processed_dataset.pkl
+
     train_users=()
     for u in "${users[@]}"; do
         if [ "$u" != "$test_user" ]; then
@@ -17,7 +25,7 @@ for test_user in "${users[@]}"; do
     echo "Running LOO: test=$test_user, train=${train_users[*]}"
 
     # Run the training command
-    python3.10 train.py \
+    $PYTHON_EXEC train.py \
         --dataset-dir ./datasets_audio \
         --split-method 2 \
         --num-time-steps 100 \
